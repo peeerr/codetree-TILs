01961606d5082a2fbde7034a195084020d344526
peeerr <ajfeh22@gmail.com>
2024-07-15 21:19:51 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240715/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,12 @@
+def f(i, j, k):
+    if i == j:
+        return k
+    if k % arr[i] == 0:
+        return f(i + 1, j, k)
+    else:
+        return f(0, j, k + 1)
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+print(f(0, n, min(arr)))
