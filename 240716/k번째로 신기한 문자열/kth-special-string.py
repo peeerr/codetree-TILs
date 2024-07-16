b30cab1544092120c0bc8ecdc096c881ee283fd3
peeerr <ajfeh22@gmail.com>
2024-07-16 15:49:07 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240716/k번째로 신기한 문자열/kth-special-string.py
@@ -0,0 +1,14 @@
+n, k, T = input().split()
+n = int(n); k = int(k)
+s = [input() for _ in range(n)]
+
+res = []
+for x in s:
+    cnt = 0
+    for i in range(len(T)):
+        if x[i] == T[i]:
+            cnt += 1
+    if cnt == len(T):
+        res.append(x)
+
+print(sorted(res)[k - 1])
