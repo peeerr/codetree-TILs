dd62af34ee8ed1bcbb3a382313e9e3cf467049f0
peeerr <ajfeh22@gmail.com>
2024-07-16 15:55:07 +0900

    Title: k번째로 신기한 문자열, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/k번째로 신기한 문자열/kth-special-string.py
+++ 240716/k번째로 신기한 문자열/kth-special-string.py
@@ -2,16 +2,10 @@ n, k, T = input().split()
 n = int(n); k = int(k)
 s = [input() for _ in range(n)]
 
-def starts_with(a, b):
-    for i in range(len(b)):
-        if a[i % len(a)] != b[i]:
-            return False
-    return True
-
 res = []
 for x in s:
     cnt = 0
-    if starts_with(x, T):
+    if x.startswith(T):
         res.append(x)
 
 print(sorted(res)[k - 1])
