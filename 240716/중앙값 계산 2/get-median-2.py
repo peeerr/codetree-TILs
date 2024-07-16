cc311385ffc3e4d53bb40f1f90afc5e0fe88a967
peeerr <ajfeh22@gmail.com>
2024-07-16 16:16:29 +0900

    Title: 중앙값 계산 2, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/중앙값 계산 2/get-median-2.py
+++ 240716/중앙값 계산 2/get-median-2.py
@@ -4,4 +4,4 @@ arr = list(map(int, input().split()))
 for i in range(len(arr)):
     length = i + 1
     if length % 2 == 1:
-        print(sorted(arr[0:length])[length // 2], end=' ')
+        print(sorted(arr[:length])[length // 2], end=' ')
