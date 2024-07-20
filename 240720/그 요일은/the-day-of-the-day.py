94ac200aee1686f82b6729c680746f0e6ee67fdd
peeerr <ajfeh22@gmail.com>
2024-07-20 13:47:31 +0900

    Title: 그 요일은, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240720/그 요일은/the-day-of-the-day.py
+++ 240720/그 요일은/the-day-of-the-day.py
@@ -4,15 +4,8 @@ a = input()
 dates = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-diff = (sum(dates[1: m2 + 1]) + d2) - (sum(dates[1: m1 + 1]) + d1) + 1
+diff = (sum(dates[1: m2 + 1]) + d2) - (sum(dates[1: m1 + 1]) + d1)
 
 idx = days.index(a)
 
 print(((diff - idx) // 7) + 1)
-
-# cnt = 0
-# for k in range(diff + 1):
-#     if days[k % 7] == a:
-#         cnt += 1
-
-# print(cnt)
