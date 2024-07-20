c4428d8a0ccb26dac2f4c704e670b1b2a234b82a
peeerr <ajfeh22@gmail.com>
2024-07-20 15:34:57 +0900

    Title: 십진수와 이진수 2, Time: 181ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/십진수와 이진수 2/decimal-and-binary-number-2.py
+++ 240720/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -1,26 +1,24 @@
-# def binary_to_digit(b):
-#     res = 0
-#     for x in b:
-#         res = res * 2 + x
-#     return res
+def binary_to_digit(b):
+    res = 0
+    for x in b:
+        res = res * 2 + x
+    return res
 
 
-# def digit_to_binary(n):
-#     res = ''
-#     while True:
-#         if n < 2:
-#             res += str(n)
-#             break
-#         res += str(n % 2)
-#         n //= 2
-#     return int(res[::-1])
+def digit_to_binary(n):
+    res = ''
+    while True:
+        if n < 2:
+            res += str(n)
+            break
+        res += str(n % 2)
+        n //= 2
+    return int(res[::-1])
 
 
-binary = bin(int(input(), 2) * 17)
+binary = list(map(int, list(input())))
 
-print(binary[2:])
+d = binary_to_digit(binary) * 17
+b = digit_to_binary(d)
 
-# d = binary_to_digit(binary) * 17
-# b = digit_to_binary(d)
-
-# print(b)
+print(b)
