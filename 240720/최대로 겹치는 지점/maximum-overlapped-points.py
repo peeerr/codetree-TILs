f10844b5f39fea93129c3f1bfd63d94cbe98a366
peeerr <ajfeh22@gmail.com>
2024-07-20 16:45:02 +0900

    Title: 최대로 겹치는 지점, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 240720/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 240720/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -1,8 +1,7 @@
 n = int(input())
 lines = [list(map(int, input().split())) for _ in range(n)]
 
-max_x = max(max(lines, key=lambda x : x[1]))
-arr = [0 for _ in range(max_x + 1)]
+arr = [0 for _ in range(101)]
 
 for line in lines:
     for i in range(line[0], line[1] + 1):
