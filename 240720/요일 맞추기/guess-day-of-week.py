ac59ce74a6e1c019e22f560f7428fd6a74630036
peeerr <ajfeh22@gmail.com>
2024-07-20 12:58:27 +0900

    Title: 요일 맞추기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/요일 맞추기/guess-day-of-week.py
+++ 240720/요일 맞추기/guess-day-of-week.py
@@ -1,16 +1,15 @@
+def cal_days(month, day):
+    res = 0
+    for i in range(1, month):
+        res += dates[i]
+    return res + day 
+
+
 m1, d1, m2, d2 = map(int, input().split())
 
 dates = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-res = 0
-if m1 <= m2:
-    for i in range(m1, m2):
-        res += dates[i]
-    res += d2 - d1
-else:
-    for i in range(m2, m1):
-        res -= dates[i]
-    res += d2 - d1
-    
-print(days[res % 7])
+diff = cal_days(m2, d2) - cal_days(m1, d1)
+
+print(days[diff % 7])
