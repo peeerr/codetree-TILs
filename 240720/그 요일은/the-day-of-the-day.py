fa5fdbf9f8b3a06a0a2fb6780e654cb90f391843
peeerr <ajfeh22@gmail.com>
2024-07-20 14:16:14 +0900

    Title: 그 요일은, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/그 요일은/the-day-of-the-day.py
+++ 240720/그 요일은/the-day-of-the-day.py
@@ -4,7 +4,7 @@ a = input()
 dates = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-diff = (sum(dates[1: m2 + 1]) + d2) - (sum(dates[1: m1 + 1]) + d1)
+diff = (sum(dates[1: m2]) + d2) - (sum(dates[1: m1]) + d1)
 
 idx = days.index(a)
 
