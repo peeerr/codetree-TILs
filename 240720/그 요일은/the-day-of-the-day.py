5107409cb8e0b76bdc664baff625aac601e37312
peeerr <ajfeh22@gmail.com>
2024-07-20 13:41:07 +0900

    Title: 그 요일은, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240720/그 요일은/the-day-of-the-day.py
+++ 240720/그 요일은/the-day-of-the-day.py
@@ -1,22 +1,18 @@
-def cal_date(month, day):
-    dates = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-    res = 0
-    for i in range(1, month):
-        res += dates[i]
-    return res + day
-
-
 m1, d1, m2, d2 = map(int, input().split())
 a = input()
 
-diff = cal_date(m2, d2) - cal_date(m1, d1)
-
+dates = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-cnt = 0
-for k in range(diff + 1):
-    if days[k % 7] == a:
-        cnt += 1
+diff = (sum(dates[1: m2 + 1]) + d2) - (sum(dates[1: m1 + 1]) + d1)
 
-print(cnt)
+idx = days.index(a)
+
+print(((diff - idx) // 7) + 1)
+
+# cnt = 0
+# for k in range(diff + 1):
+#     if days[k % 7] == a:
+#         cnt += 1
+
+# print(cnt)
