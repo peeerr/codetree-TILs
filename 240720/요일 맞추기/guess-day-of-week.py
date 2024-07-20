988ead397c47328f39187bf956048c643e91f1e6
peeerr <ajfeh22@gmail.com>
2024-07-20 12:52:41 +0900

    Title: 요일 맞추기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/요일 맞추기/guess-day-of-week.py
+++ 240720/요일 맞추기/guess-day-of-week.py
@@ -4,8 +4,13 @@ dates = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 res = 0
-for i in range(min(m1, m2), max(m1, m2)):
-    res += dates[i]
-res += d2 - d1
-
+if m1 <= m2:
+    for i in range(m1, m2):
+        res += dates[i]
+    res += d2 - d1
+else:
+    for i in range(m2, m1):
+        res -= dates[i]
+    res += d2 - d1
+    
 print(days[res % 7])
