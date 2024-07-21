f1b5c9167f1585e3cab610d627c8e756c007c1db
peeerr <ajfeh22@gmail.com>
2024-07-21 16:48:05 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 288ms, Memory: 55MB, Status: Passed - CodeTree

--- 240721/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 240721/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -2,22 +2,17 @@ rects = [list(map(lambda x : int(x) + 1000, input().split())) for _ in range(3)]
 
 grid = [[0 for _ in range(2000)] for _ in range(2000)]
 
-for n in range(3):
-    x1, y1, x2, y2 = rects[n]
+for n, rect in enumerate(rects, start=1):
+    x1, y1, x2, y2 = rect
 
-    if n == 2:
-        for i in range(y1, y2):
-            for j in range(x1, x2):
-                if grid[i][j] == 1:
-                    grid[i][j] = 0
-        break
-        
     for i in range(y1, y2):
         for j in range(x1, x2):
-            grid[i][j] = 1
+            grid[i][j] = n
 
 res = 0
-for x in grid:
-    res += sum(x)
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        if grid[i][j] == 1 or grid[i][j] == 2:
+            res += 1
 
 print(res)
