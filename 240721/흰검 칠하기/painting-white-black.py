8c2206c212de7ca21a54817f627472c55b6ad404
peeerr <ajfeh22@gmail.com>
2024-07-21 15:35:16 +0900

    Title: 흰검 칠하기, Time: 369ms, Memory: 53MB, Status: Passed - CodeTree

--- 240721/흰검 칠하기/painting-white-black.py
+++ 240721/흰검 칠하기/painting-white-black.py
@@ -3,24 +3,24 @@ n = int(input())
 grid = [['', [0, 0]] for _ in range(200001)]
 
 start_pos = 100000
-color = ''
+colour = ''
 direction = 0
 for _ in range(n):
     instructions = input().split()
     instructions[0] = int(instructions[0])
 
     if instructions[1] == 'L':
-        color = 'W'
+        colour = 'W'
         direction = -1
     else:
-        color = 'B'
+        colour = 'B'
         direction = 1
 
     for i in range(start_pos, (start_pos + (instructions[0] * direction)), direction):
-        if grid[i][0] != color and grid[i][0] != 'G':
-            grid[i][0] = color
+        if grid[i][0] != colour and grid[i][0] != 'G':
+            grid[i][0] = colour
 
-        if color == 'W':
+        if colour == 'W':
             grid[i][1][0] += 1
         else:
             grid[i][1][1] += 1
