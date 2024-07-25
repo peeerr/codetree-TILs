a4653db75230195440df7f3823a4b9ec991ed5b0
peeerr <ajfeh22@gmail.com>
2024-07-25 18:46:36 +0900

    Title: 벌금은 누구에게, Time: 183ms, Memory: 77MB, Status: Passed - CodeTree

--- 240725/벌금은 누구에게/who-will-pay.py
+++ 240725/벌금은 누구에게/who-will-pay.py
@@ -1,11 +1,16 @@
+def f():
+    for target in targets:
+        students[target] += 1
+
+        if students[target] == k:
+            return target
+
+    return -1
+
+
 n, m, k = map(int, input().split())
 targets = [int(input()) for _ in range(m)]
 
 students = [0 for _ in range(n + 1)]
 
-for target in targets:
-    students[target] += 1
-
-    if students[target] == k:
-        print(target)
-        break
+print(f())
