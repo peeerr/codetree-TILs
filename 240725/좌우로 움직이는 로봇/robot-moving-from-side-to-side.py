e8f942955c099eb59cefcb67f2915c4023f3f87c
peeerr <ajfeh22@gmail.com>
2024-07-25 20:02:13 +0900

    Title: 좌우로 움직이는 로봇, Time: 304ms, Memory: 63MB, Status: Passed - CodeTree

--- 240725/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240725/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,35 +1,28 @@
 n, m = map(int, input().split())
 
-arr1, pos = [0], 1
+arr1, pos1 = [0], 1
 for _ in range(n):
     t, d = input().split(); t = int(t)
-
     for _ in range(t):
-        arr1.append(arr1[pos - 1] + (1 if d == 'R' else -1))
-        pos += 1
+        arr1.append(arr1[pos1 - 1] + (1 if d == 'R' else -1))
+        pos1 += 1
 
-arr2, pos = [0], 1
+arr2, pos2 = [0], 1
 for _ in range(m):
     t, d = input().split(); t = int(t)
-
     for _ in range(t):
-        arr2.append(arr2[pos - 1] + (1 if d == 'R' else -1))
-        pos += 1
+        arr2.append(arr2[pos2 - 1] + (1 if d == 'R' else -1))
+        pos2 += 1
 
-last, cnt, more = float('inf'), 0, 0
-for i in range(1, max(len(arr1), len(arr2)) - 1):
-    if i + 1 == len(arr1):
-        last = arr1[i]
-        more = 2
-    elif i + 1 == len(arr2):
-        last = arr2[i]
-        more = 1
+while len(arr1) != len(arr2):
+    if len(arr1) < len(arr2):
+        arr1.append(arr1[-1])
+    else:
+        arr2.append(arr2[-1])
 
-    if more == 0 and arr1[i] != arr2[i] and arr1[i + 1] == arr2[i + 1]:
-        cnt += 1
-    elif more == 1 and arr1[i + 1] == last:
-        cnt += 1
-    elif more == 2 and arr2[i + 1] == last:
+cnt = 0
+for i in range(1, len(arr1) - 1):
+    if arr1[i] != arr2[i] and arr1[i + 1] == arr2[i + 1]:
         cnt += 1
 
 print(cnt)
