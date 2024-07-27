b0e9a3efff93fb9dea0ed4dbcf1b544d887cac08
peeerr <ajfeh22@gmail.com>
2024-07-27 17:10:21 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240727/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240727/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -6,19 +6,28 @@ n = int(input())
 
 grid = [[0 for _ in range(n)] for _ in range(n)]
 
-dx, dy = [0, -1, 0, 1], [-1, 0, 1, 0]
-x, y = n - 1, n - 1
-direction = 0
+dx, dy = [0, -1, 0, 1], [1, 0, -1, 0]
+x, y = n // 2, n // 2
+direction, distance = 0, 0
+end = False
 
-for num in range(n ** 2, 0, -1):
-    grid[x][y] = num
+num = 1
+while True:
+    if direction == 0 or direction == 2:
+        distance += 1
 
-    nx, ny = x + dx[direction], y + dy[direction]
+    for _ in range(distance):
+        grid[x][y] = num
+        num += 1
+        x, y = x + dx[direction], y + dy[direction]
 
-    if not in_range(nx, ny) or grid[nx][ny] != 0:
-        direction = (direction + 1) % 4
+        if not in_range(x, y):
+            end = True
+            break
 
-    x, y = x + dx[direction], y + dy[direction]
+    if end:
+        break
+    direction = (direction + 1) % 4
 
 for i in range(n):
     for j in range(n):
