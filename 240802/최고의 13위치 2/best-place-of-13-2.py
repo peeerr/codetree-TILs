b5d836d3540fa1a3f0aa57fec4841e13e83dd950
peeerr <ajfeh22@gmail.com>
2024-08-02 21:36:47 +0900

    Title: 최고의 13위치 2, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/최고의 13위치 2/best-place-of-13-2.py
+++ 240802/최고의 13위치 2/best-place-of-13-2.py
@@ -7,9 +7,7 @@ for i in range(n):
     for j in range(n - 2):
         for k in range(n):
             for l in range(n - 2):
-                if i == k:
-                    l = j + 3
-                if l >= n - 2:
+                if i == k and j + 3 >= n - 2:
                     continue
                 res = max(res, grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + grid[k][l] + grid[k][l + 1] + grid[k][l + 2])
 
