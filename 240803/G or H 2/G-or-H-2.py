bacff6b01ab3e83ae965b1e4cc07ad7b8bad1c71
peeerr <ajfeh22@gmail.com>
2024-08-03 19:36:12 +0900

    Title: G or H 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/G or H 2/G-or-H-2.py
@@ -0,0 +1,25 @@
+n = int(input())
+infos = [list(map(lambda x : int(x) if x.isdigit() else x, input().split())) for _ in range(n)]
+
+length = max(infos, key=lambda x : x[0])[0]
+arr = [0 for _ in range(length + 1)]
+
+for info in infos:
+    arr[info[0]] = info[1]
+
+res = 0
+
+for i in range(1, length + 1):
+    if arr[i] in ['G', 'H']:
+        for j in range(1, length + 1 - i):
+            if arr[i + j - 1] in ['G', 'H']:
+                g, h = 0, 0
+                for k in range(i, i + j):
+                    if arr[k] == 'G':
+                        g += 1
+                    elif arr[k] == 'H':
+                        h += 1
+                if (g != 0 and h != 0 and g == h) or (g == 0 and h == j) or (h == 0 and g == j):
+                    res = max(res, j)
+            
+print(res - 1 if res != 0 else res)
