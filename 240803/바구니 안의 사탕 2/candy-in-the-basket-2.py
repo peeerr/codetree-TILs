6c2ee6ac53f253c15ddd814f57b105555d6df7f6
peeerr <ajfeh22@gmail.com>
2024-08-03 18:13:52 +0900

    Title: 바구니 안의 사탕 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+arr = [0 for _ in range(101)]
+
+for _ in range(n):
+    info = tuple(map(int, input().split()))
+    arr[info[1]] = info[0]
+
+res = 0
+
+for i in range(1 + k, 101 - k):
+    tmp = 0
+    for j in range(i - k, i + k + 1):
+        tmp += arr[j]
+    res = max(res, tmp)
+
+print(res)
