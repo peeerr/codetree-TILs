3d339f2d5d517f2b951b5be94185c4ebf4999cbd
peeerr <ajfeh22@gmail.com>
2024-08-03 20:02:12 +0900

    Title: G or H 2, Time: 205ms, Memory: 25MB, Status: Passed - CodeTree

--- 240803/G or H 2/G-or-H-2.py
+++ 240803/G or H 2/G-or-H-2.py
@@ -8,16 +8,18 @@ for _ in range(n):
 res = 0
 
 for i in range(1, 101):
-    if arr[i] in ['G', 'H']:
-        for j in range(1, 101 - i):
-            if arr[i + j] in ['G', 'H']:
-                g, h = 0, 0
-                for k in range(i, i + j + 1):
-                    if arr[k] == 'G':
-                        g += 1
-                    elif arr[k] == 'H':
-                        h += 1
-                if (g == h != 0) or (g == 0 and h > 0) or (h == 0 and g > 0):
-                    res = max(res, j)
+    for j in range(i + 1, 101):
+        if arr[i] == 0 or arr[j] == 0:
+            continue
+        
+        g, h = 0, 0
+        for k in range(i, j + 1):
+            if arr[k] == 'G':
+                g += 1
+            elif arr[k] == 'H':
+                h += 1
+
+        if g == h or g == 0 or h == 0:
+            res = max(res, j - i)
             
 print(res)
