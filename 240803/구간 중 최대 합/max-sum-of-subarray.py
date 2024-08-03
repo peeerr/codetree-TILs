fab82a245e1e0f78becbfaa6bc2126e48773d443
peeerr <ajfeh22@gmail.com>
2024-08-03 16:22:37 +0900

    Title: 구간 중 최대 합, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/구간 중 최대 합/max-sum-of-subarray.py
+++ 240803/구간 중 최대 합/max-sum-of-subarray.py
@@ -4,8 +4,8 @@ arr = list(map(int, input().split()))
 res = 0
 
 for i in range(n - k + 1):
-    tmp = arr[i]
-    for j in range(i + 1, i + k):
+    tmp = 0
+    for j in range(i, i + k):
         tmp += arr[j]
     res = max(res, tmp)
 
