a12a4329301af06833b02bec854fced013883725
peeerr <ajfeh22@gmail.com>
2024-08-04 14:45:40 +0900

    Title: 개발자의 능력 3, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240804/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,13 @@
+import sys
+
+arr = list(map(int, input().split()))
+
+res = sys.maxsize
+
+for i in range(6):
+    for j in range(6):
+        for k in range(6):
+            team_sum = arr[i] + arr[j] + arr[k]
+            res = min(res, abs(team_sum - (sum(arr) - team_sum)))
+
+print(res)
