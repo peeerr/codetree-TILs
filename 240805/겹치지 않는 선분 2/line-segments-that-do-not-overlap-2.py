0298b29bc2c5683355859bdacefeea38d459affc
peeerr <ajfeh22@gmail.com>
2024-08-05 16:57:11 +0900

    Title: 겹치지 않는 선분 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240805/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240805/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -11,7 +11,12 @@ for i in range(n):
         x1, x2 = arr[i]
         x3, x4 = arr[j]
 
-        if (x3 <= x1 <= x4 and x3 <= x2 <= x4) or (x1 <= x3 <= x2 and x1 <= x4 <= x2):
+        # if x1 == x3 or x1 == x4 or x2 == x3 or x2 == x4:
+        #     is_ok = False
+        #     break
+
+        if (x3 <= x1 <= x4 and x3 <= x2 <= x4) or (x1 <= x3 <= x2 and x1 <= x4 <= x2) or \
+            (x4 <= x1 <= x3 and x4 <= x2 <= x3) or (x2 <= x3 <= x1 and x2 <= x4 <= x1):
             is_ok = False
             break
 
