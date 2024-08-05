a57a21248f6bda70143c5aa4348db1c392eb10f1
peeerr <ajfeh22@gmail.com>
2024-08-05 13:50:36 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240805/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+min_distance = sys.maxsize
+
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            continue
+        
+        distance = (((arr[i][0] - arr[j][0]) ** 2) + ((arr[i][1] - arr[j][1]) ** 2)) ** 0.5
+        
+        if distance < min_distance:
+            min_distance = distance
+
+print(int(min_distance ** 2))
