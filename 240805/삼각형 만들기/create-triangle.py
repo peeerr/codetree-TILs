d2a27e780848b19af9f0627d3c9f4a3ed62c251f
peeerr <ajfeh22@gmail.com>
2024-08-05 16:20:22 +0900

    Title: 삼각형 만들기, Time: 283ms, Memory: 27MB, Status: Passed - CodeTree

--- 240805/삼각형 만들기/create-triangle.py
+++ 240805/삼각형 만들기/create-triangle.py
@@ -1,3 +1,9 @@
+def check(i, j, k):
+    if (arr[i][0] == arr[j][0] or arr[i][0] == arr[k][0] or arr[j][0] == arr[k][0]) and (arr[i][1] == arr[j][1] or arr[i][1] == arr[k][1] or arr[j][1] == arr[k][1]):
+        return True
+    return False
+
+
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 res = 0
@@ -5,13 +11,10 @@ res = 0
 for i in range(n):
     for j in range(n):
         for k in range(n):
-            if i == j or i == k or j == k:
+            if i == j or i == k or j == k or not check(i, j, k):
                 continue
-            
 
-            area = 0.5 * abs((arr[i][0] * arr[j][1] + arr[j][0] * arr[k][1] + arr[k][0] * arr[i][1]) - (arr[j][1] * arr[i][1] + arr[k][0] * arr[j][1] + arr[i][0] * arr[k][1]))
+            area = abs((arr[i][0] * arr[j][1] + arr[j][0] * arr[k][1] + arr[k][0] * arr[i][1]) - (arr[j][0] * arr[i][1] + arr[k][0] * arr[j][1] + arr[i][0] * arr[k][1]))
+            res = max(res, area)
 
-            if area == int(area):
-                res = max(res, int(area))
-
-print(res * 2)
+print(res)
