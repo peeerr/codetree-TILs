c80ae0009d318cb57659dacc27b50e8e788d389e
peeerr <ajfeh22@gmail.com>
2024-09-09 15:04:30 +0900

    Title: 개발자의 순위, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240909/개발자의 순위/developer's-rank.py
+++ 240909/개발자의 순위/developer's-rank.py
@@ -1,19 +1,17 @@
 k, n = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(k)]
 
-cnt = 0
+res = 0
 lst = []
 
 for i in range(n):
     for j in range(i + 1, n):
-        lst.append((arr[0][i], arr[0][j]))
+        a, b = arr[0][i], arr[0][j]
+        cnt = 0
+        for l in range(k):
+            if b in arr[l][arr[l].index(a) + 1:]:
+                cnt += 1
+        if cnt == k:
+            res += 1
 
-for x in lst:
-    temp = 0
-    for y in arr:
-        if x[1] in y[y.index(x[0]) + 1 :]:
-            temp += 1
-    if temp == k:
-        cnt += 1
-
-print(cnt)
+print(res)
