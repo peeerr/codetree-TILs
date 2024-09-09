42fba082362cac65284c7dea42d8d5d8c5a2b9a4
peeerr <ajfeh22@gmail.com>
2024-09-09 17:19:16 +0900

    Title: 상해버린 치즈, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240909/상해버린 치즈/rotten-cheese.py
+++ 240909/상해버린 치즈/rotten-cheese.py
@@ -3,21 +3,23 @@ eat_record = [list(map(int, input().split())) for _ in range(d)]
 sick_record = [list(map(int, input().split())) for _ in range(s)]
 
 
-spoiled_cheese = set()
+spoiled_cheese = []
 
 for cheese in range(1, m + 1):
 
     cnt = 0
+    temp = []
 
-    for sick in sick_record:
-        sick_person, sick_time = sick[0], sick[1]
-
-        for eat in eat_record:
-            if sick_person == eat[0] and cheese == eat[1] and sick_time > eat[2]:
+    for eat in eat_record:
+        if eat[0] in temp:
+            continue
+        for sick in sick_record:
+            if sick[0] == eat[0] and cheese == eat[1] and sick[1] > eat[2]:
+                temp.append(eat[0])
                 cnt += 1
     
     if cnt == s:
-        spoiled_cheese.add(cheese)
+        spoiled_cheese.append(cheese)
 
 
 res_person = set()
