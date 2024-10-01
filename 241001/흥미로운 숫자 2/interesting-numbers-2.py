7f7cb9f57926e4df88cf5b9ca6dabd9d42aabcc5
peeerr <ajfeh22@gmail.com>
2024-10-01 17:06:00 +0900

    Title: 흥미로운 숫자 2, Time: 131ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241001/흥미로운 숫자 2/interesting-numbers-2.py
+++ 241001/흥미로운 숫자 2/interesting-numbers-2.py
@@ -3,7 +3,19 @@ x, y = map(int, input().split())
 ans = 0
 
 for num in range(x, y + 1):
-    if len(set(str(num))) == 2:
+    tmp = [0 for _ in range(10)]
+
+    for x in map(int, str(num)):
+        tmp[x] += 1
+
+    success = 0
+    for x in tmp:
+        if x >= 2:
+            success += 1
+        elif x == 1:
+            success += 1
+
+    if success == 2:
         ans += 1
 
 print(ans)
