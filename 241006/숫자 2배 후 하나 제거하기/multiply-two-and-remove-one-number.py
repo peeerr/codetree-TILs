21f31dccbd5a66caf7989786c1717b6371797fd1
peeerr <ajfeh22@gmail.com>
2024-10-06 17:55:23 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 241006/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
+++ 241006/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -9,16 +9,12 @@ for i in range(n):
     for j in range(n):
         s = 0
 
-        for k in range(n - 1):
-            if k == j:
-                continue
+        tmp = arr[:]
+        tmp[i] *= 2
+        tmp.pop(j)
 
-            if k == i:
-                s += abs((arr[k] * 2) - arr[k + 1])
-            elif k + 1 == i:
-                s += abs(arr[k] - (arr[k + 1] * 2))
-            else:
-                s += abs(arr[k] - arr[k + 1])
+        for k in range(len(tmp) - 1):
+            s += abs(tmp[k] - tmp[k + 1])
 
         ans = min(ans, s)
 
