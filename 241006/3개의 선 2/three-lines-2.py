303f8bf706815e65516f961b97b4026cf98aa845
peeerr <ajfeh22@gmail.com>
2024-10-06 18:40:48 +0900

    Title: 3개의 선 2, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241006/3개의 선 2/three-lines-2.py
+++ 241006/3개의 선 2/three-lines-2.py
@@ -4,16 +4,19 @@ positions = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 
 for i in range(11):
-    for j in range(i + 1, 11):
-        for k in range(j + 1, 11):
+    for x_y1 in range(2):
+        for j in range(i + 1, 11):
+            for x_y2 in range(2):
+                for k in range(j + 1, 11):
+                    for x_y3 in range(2):
 
-            success = True
-            
-            for pos in positions:
-                if pos[0] not in [i, j, k] and pos[1] not in [i, j, k]:
-                    success = False
-            
-            if success:
-                ans = 1
+                        success = True
+                        
+                        for pos in positions:
+                            if pos[x_y1] != i and pos[x_y2] != j and pos[x_y3] != k:
+                                success = False
+                        
+                        if success:
+                            ans = 1
 
 print(ans)
