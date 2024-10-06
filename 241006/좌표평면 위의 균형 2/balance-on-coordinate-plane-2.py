44cba238d3458e798d1de3c789169b10b164b058
peeerr <ajfeh22@gmail.com>
2024-10-06 21:15:23 +0900

    Title: 좌표평면 위의 균형 2, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241006/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ 241006/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -5,21 +5,22 @@ positions = [list(map(int, input().split())) for _ in range(n)]
 
 ans = sys.maxsize
 
-for i in range(101):
-    for j in range(101):
+for i in range(0, 101, 2):
+    for j in range(0, 101, 2):
         
         a1, a2, a3, a4 = 0, 0, 0, 0
 
         for x, y in positions:
-            if x < i and y < j:
+            if x > i and y > j:
                 a1 += 1
-            elif x > i and y < j:
+            elif x < i and y > j:
                 a2 += 1
-            elif x > i and y > j:
+            elif x < i and y < j:
                 a3 += 1
-            else:
+            elif x > i and y < j:
                 a4 += 1
 
-        ans = min(ans, max(a1, a2, a3, a4))
+        if a1 and a2 and a3 and a4:
+            ans = min(ans, max(a1, a2, a3, a4))
 
 print(ans)
