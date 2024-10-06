10fba78d186f10144ef1240695a6587313d31eea
peeerr <ajfeh22@gmail.com>
2024-10-06 16:44:46 +0900

    Title: 야바위, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241006/야바위/ya-rock.py
+++ 241006/야바위/ya-rock.py
@@ -6,10 +6,12 @@ ans = 0
 for i in range(1, 4):
     score = 0
 
-    for numbers in arr:
+    for tmp in arr:
+        numbers = tmp[:]
+        
         numbers[0], numbers[1] = numbers[1], numbers[0]
 
-        if numbers[numbers[2]] == i:
+        if numbers[numbers[2] - 1] == i:
             score += 1
 
     ans = max(ans, score)
