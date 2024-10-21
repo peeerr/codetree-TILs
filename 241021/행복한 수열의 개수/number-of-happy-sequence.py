5c0de55ccaf924c5fdd228557ddd6ba933120e05
peeerr <ajfeh22@gmail.com>
2024-10-21 15:33:08 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241021/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241021/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,34 +1,27 @@
+def is_happy(lst):
+    cnt, max_cnt = 1, 1
+
+    for i in range(n - 1):
+        if grid[i] == grid[i + 1]:
+            cnt += 1
+        max_cnt = max(max_cnt, cnt)
+    
+    if max_cnt >= m:
+        return 1
+
+
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 ans = 0
 
 for i in range(n):
-    for j in range(n):
+    ans += is_happy(grid[i][:])
 
-        if i == 0:
-            cnt = 1
-            for k in range(i, n - 1):
-                if grid[k][j] == grid[k + 1][j]:
-                    cnt += 1
-                else:
-                    cnt = 1
-                if cnt >= m:
-                    ans += 1
-                    break
+for j in range(n):
+    lst = []
+    for i in range(n):
+        lst.append(grid[i][j])
+    ans += is_happy(lst)
 
-        if j == 0:
-            cnt = 1
-            for k in range(j, n - 1):
-                if grid[i][k] == grid[i][k + 1]:
-                    cnt += 1
-                else:
-                    cnt = 1
-                if cnt >= m:
-                    ans += 1
-                    break
-
-if n == 1:
-    print(2)
-else:
-    print(ans)
+print(ans)
