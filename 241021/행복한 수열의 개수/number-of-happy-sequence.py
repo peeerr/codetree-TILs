0d85f6e8717c0e4ef31be6baeb663027b50841db
peeerr <ajfeh22@gmail.com>
2024-10-21 15:14:11 +0900

    Title: 행복한 수열의 개수, Time: 124ms, Memory: 28MB, Status: Passed - CodeTree

--- 241021/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241021/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,29 +1,3 @@
-def is_happy(i, j):
-    ret_Val = 0
-
-    if i == 0:
-        cnt = 1
-        for k in range(i, n - 1):
-            if grid[k][j] == grid[k + 1][j]:
-                cnt += 1
-            else:
-                cnt = 1
-        if cnt >= m:
-            ret_Val += 1
-
-    if j == 0:
-        cnt = 1
-        for k in range(j, n - 1):
-            if grid[i][k] == grid[i][k + 1]:
-                cnt += 1
-            else:
-                cnt = 1
-        if cnt >= m:
-            ret_Val += 1
-
-    return ret_Val
-
-
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
@@ -31,6 +5,30 @@ ans = 0
 
 for i in range(n):
     for j in range(n):
-        ans += is_happy(i, j)
 
-print(ans)
+        if i == 0:
+            cnt = 1
+            for k in range(i, n - 1):
+                if grid[k][j] == grid[k + 1][j]:
+                    cnt += 1
+                else:
+                    cnt = 1
+                if cnt >= m:
+                    ans += 1
+                    break
+
+        if j == 0:
+            cnt = 1
+            for k in range(j, n - 1):
+                if grid[i][k] == grid[i][k + 1]:
+                    cnt += 1
+                else:
+                    cnt = 1
+                if cnt >= m:
+                    ans += 1
+                    break
+
+if n == 1:
+    print(2)
+else:
+    print(ans)
