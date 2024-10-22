91fe9bf218fc605d075828c046d360e35c925197
peeerr <ajfeh22@gmail.com>
2024-10-22 19:29:43 +0900

    Title: 금 채굴하기, Time: 214ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 241022/금 채굴하기/gold-mining.py
+++ 241022/금 채굴하기/gold-mining.py
@@ -11,31 +11,15 @@ for k in range(n):
     
     for i in range(n):
         for j in range(n):
-
-            if k == 0:
-                ans = max(ans, grid[i][j])
-                continue
-
+            
             cost, gold = 0, 0
 
-            for l in range(i - k + 1, i + k):
-                for o in range(j - k + 1, j + k):
-                    cost += 1
-                    if not in_range(l, o):
-                        continue
-                    gold += grid[l][o]
-                    if i == 1 and j == 1 and k == 0:
-                        test[l][o] += grid[l][o]
-            
-            if in_range(i - k, j):
-                gold += grid[i - k][j]
-            if in_range(i + k, j):
-                gold += grid[i + k][j]
-            if in_range(i, j - k):
-                gold += grid[i][j - k]
-            if in_range(i, j + k):
-                gold += grid[i][j + k]
-            cost += 4
+            for l in range(-100, 100):
+                for o in range(-100, 100):
+                    if abs(l - i) + abs(o - j) <= k:
+                        cost += 1
+                        if in_range(l, o):
+                            gold += grid[l][o]
 
             if cost <= m * gold:
                 ans = max(ans, gold)
