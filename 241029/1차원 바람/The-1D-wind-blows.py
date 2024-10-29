4e2ba0e647d05f81a1aaaa29d8d033b70a4e53e9
peeerr <ajfeh22@gmail.com>
2024-10-29 16:55:09 +0900

    Title: 1차원 바람, Time: 136ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 241029/1차원 바람/The-1D-wind-blows.py
+++ 241029/1차원 바람/The-1D-wind-blows.py
@@ -18,15 +18,7 @@ def check_spread(r1, r2):
     return False
 
 
-n, m, q = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
-winds = [list(map(lambda x : int(x) - 1 if x.isdigit() else x, input().split())) for _ in range(q)]
-
-for r, d in winds:
-    shift(r, d)
-    down_r = r
-    down_d = d
-
+def spread_up(r, d):
     while True:
         is_continue = False
         for i in range(r - 1, -1, -1):
@@ -39,17 +31,29 @@ for r, d in winds:
         if not is_continue:
             break
 
+
+def spread_down(r, d):
     while True:
         is_continue = False
-        for i in range(down_r + 1, n):
-            if check_spread(down_r, i):
-                down_d = 'L' if down_d == 'R' else 'R'
-                shift(i, down_d)
-                down_r = i
+        for i in range(r + 1, n):
+            if check_spread(r, i):
+                d = 'L' if d == 'R' else 'R'
+                shift(i, d)
+                r = i
                 is_continue = True
                 break
         if not is_continue:
             break
+
+
+n, m, q = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+winds = [list(map(lambda x : int(x) - 1 if x.isdigit() else x, input().split())) for _ in range(q)]
+
+for r, d in winds:
+    shift(r, d)
+    spread_up(r, d)
+    spread_down(r, d)
 
 for i in range(n):
     for j in range(m):
