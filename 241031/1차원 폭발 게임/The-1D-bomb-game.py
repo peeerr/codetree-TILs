57144ffa584903736bb7561bd07e54d3f3a1f174
peeerr <ajfeh22@gmail.com>
2024-10-31 16:22:39 +0900

    Title: 1차원 폭발 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241031/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,44 @@
+def cut(m):
+    global n
+
+    temp = []
+    i = 0
+    is_cut = False
+
+    while i < n:
+        if arr[i] == arr[i + 1]:
+            idx, cnt = i + 1, 2
+
+            while idx < n - 1:
+                if arr[idx] != arr[idx + 1]:
+                    break
+                cnt += 1
+                idx += 1
+            
+            if cnt >= m:
+                is_cut = True
+                i = idx + 1
+        
+        if i < n:
+            temp.append(arr[i])
+        i += 1
+    
+    n = len(temp)
+    for i in range(n):
+        arr[i] = temp[i]
+
+    return is_cut
+
+
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+while cut(m):
+    pass
+
+if n:
+    print(n)
+    for i in range(n):
+        print(arr[i])
+else:
+    print(0)
