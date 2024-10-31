4cd0e4a90d9822e5409c3e2c1623a133f5602ec9
peeerr <ajfeh22@gmail.com>
2024-10-31 14:53:54 +0900

    Title: 1차원 젠가, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/1차원 젠가/jenga-1d.py
+++ 241031/1차원 젠가/jenga-1d.py
@@ -1,21 +1,24 @@
+def cut(s, e):
+    global n
+    temp = []
+
+    for i in range(s - 1):
+        temp.append(jenga[i])
+    for i in range(e, n):
+        temp.append(jenga[i])
+    
+    n = len(temp)
+    for i in range(n):
+        jenga[i] = temp[i]
+
+
 n = int(input())
 jenga = [int(input()) for _ in range(n)]
-s1, e1 = map(int, input().split())
-s2, e2 = map(int, input().split())
 
-temp1 = []
-temp2 = []
+for _ in range(2):
+    s, e = map(int, input().split())
+    cut(s, e)
 
-for i in range(s1 - 1):
-    temp1.append(jenga[i])
-for i in range(e1, n):
-    temp1.append(jenga[i])
-
-for i in range(s2 - 1):
-    temp2.append(temp1[i])
-for i in range(e2, len(temp1)):
-    temp2.append(temp1[i])
-
-print(len(temp2))
-for x in temp2:
-    print(x)
+print(n)
+for i in range(n):
+    print(jenga[i])
