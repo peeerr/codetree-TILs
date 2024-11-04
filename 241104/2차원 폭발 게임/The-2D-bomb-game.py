b5a522c081565cf939e30d2006a1150bfd5eed85
peeerr <ajfeh22@gmail.com>
2024-11-04 18:36:31 +0900

    Title: 2차원 폭발 게임, Time: 166ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 241104/2차원 폭발 게임/The-2D-bomb-game.py
+++ 241104/2차원 폭발 게임/The-2D-bomb-game.py
@@ -6,16 +6,17 @@ def bomb():
         cnt = 1
 
         for i in range(n):
-            if i + 1 < n and grid[i][j] and grid[i][j] == grid[i + 1][j]:
-                end = i + 1
-                cnt += 1
-            else:
-                if cnt >= m:
-                    is_bomb = True
-                    for k in range(start, end + 1):
-                        grid[k][j] = 0
-                start, end = i + 1, i + 1
-                cnt = 1
+            if grid[i][j]:
+                if i + 1 < n and grid[i][j] == grid[i + 1][j]:
+                    end = i + 1
+                    cnt += 1
+                else:
+                    if cnt >= m:
+                        is_bomb = True
+                        for k in range(start, end + 1):
+                            grid[k][j] = 0
+                    start, end = i + 1, i + 1
+                    cnt = 1
 
     return is_bomb
 
