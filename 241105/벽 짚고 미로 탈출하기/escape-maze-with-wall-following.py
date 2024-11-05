78a4393bfae3a47a72f95aac45b258599b0736bd
peeerr <ajfeh22@gmail.com>
2024-11-05 14:24:46 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 5043ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

---
+++ 241105/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,45 @@
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n 
+
+
+n = int(input())
+x, y = map(lambda x : int(x) - 1, input().split())
+maze = [list(input()) for _ in range(n)]
+
+start_x, start_y = x, y
+
+counterclock_dxs, counterclock_dys = [0, -1, 0, 1], [1, 0, -1, 0]
+clock_dxs, clock_dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+is_out = True
+ans = 0
+d = 0
+clockwise = True
+
+while True:
+    if clockwise:
+        nx, ny = x + clock_dxs[d], y + clock_dys[d]
+    else:
+        nx, ny = x + counterclock_dxs[d], y + counterclock_dys[d]
+
+    if not in_range(nx, ny):
+        ans += 1
+        break
+
+    if maze[nx][ny] == '#':
+        clockwise = False
+        d = (d + 1) % 4
+        continue
+    else:
+        nnx, nny = nx + clock_dxs[(d + 1) % 4], ny + clock_dys[(d + 1) % 4]
+        if not in_range(nnx, nny) or maze[nnx][nny] == '.':
+            d = (d + 1) % 4
+        x, y = nx, ny
+        ans += 1
+        clockwise = True
+
+    if x == start_x and y == start_y:
+        is_out = False
+        break
+
+print(ans) if is_out else print(-1)
