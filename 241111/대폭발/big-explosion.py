88e53a19cda7ada89dcd5f5f5ea869525b93b5f9
peeerr <ajfeh22@gmail.com>
2024-11-11 16:00:46 +0900

    Title: 대폭발, Time: 56ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241111/대폭발/big-explosion.py
@@ -0,0 +1,32 @@
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+
+n, m, r, c = map(int, input().split())
+r, c = r - 1, c - 1
+
+grid = [[0 for _ in range(n)] for _ in range(n)]
+dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+grid[r][c] = 1
+
+for t in range(1, m + 1):
+    positions = []
+
+    for x in range(n):
+        for y in range(n):
+
+            if not grid[x][y]:
+                continue
+
+            for dx, dy in zip(dxs, dys):
+                nx, ny = x + dx * t, y + dy * t
+
+                if in_range(nx, ny):
+                    positions.append((nx, ny))
+
+    for pos in positions:
+        x, y = pos
+        grid[x][y] = 1
+
+print(sum([grid[i][j] for j in range(n) for i in range(n)]))
