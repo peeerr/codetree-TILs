00e0c6c793a7620aea620b8d0a0207ef1f26b39d
peeerr <ajfeh22@gmail.com>
2024-11-12 19:09:57 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 52ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241112/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 241112/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -13,15 +13,12 @@ for x, y in positions:
     count[x][y] += 1
     next_pos.append((x, y))
 
-dxs, dys = [0, 0, 1, -1], [1, -1, 0, 0]
+dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 for _ in range(t):
     temp = []
 
     for x, y in next_pos:
-        if not count[x][y]:
-            continue
-        
         max_num = 0
         max_x, max_y = x, y
         
@@ -36,11 +33,16 @@ for _ in range(t):
         count[x][y] -= 1
         temp.append((max_x, max_y))
 
-    next_pos = [x for x in temp]
+    next_pos = set([x for x in temp])
 
-    # for x in range(n):
-    #     for y in range(n):
-    #         if count[x][y] > 1:
-    #             count[x][y] = 0
+    # 충돌한 구슬 제거
+    for x in range(n):
+        for y in range(n):
+            if count[x][y] > 1:
+                count[x][y] = 0
+                next_pos.remove((x, y))
 
+for i in range(n):
+    print(*count[i])
+print()
 print(sum([1 if count[x][y] == 1 else 0 for y in range(n) for x in range(n)]))
