3333a04d46130d181e6a5ba0e28db1d0085b5ba3
peeerr <ajfeh22@gmail.com>
2024-11-12 17:23:32 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 85ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 241112/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 241112/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -7,34 +7,40 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 positions = [tuple(map(lambda x : int(x) - 1, input().split())) for _ in range(m)]
  
 count = [[0 for _ in range(n)] for _ in range(n)]
+next_pos = []
 
 for x, y in positions:
     count[x][y] += 1
+    next_pos.append((x, y))
 
-dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+dxs, dys = [0, 0, 1, -1], [1, -1, 0, 0]
 
 for _ in range(t):
-    for x in range(n):
-        for y in range(n):
-            if not count[x][y]:
-                continue
-            
-            max_num = 0
-            max_x, max_y = x, y
-            
-            for dx, dy in zip(dxs, dys):
-                nx, ny = x + dx, y + dy
+    temp = []
 
-                if in_range(nx, ny) and grid[nx][ny] > max_num:
-                    max_num = grid[nx][ny]
-                    max_x, max_y = nx, ny
+    for x, y in next_pos:
+        if not count[x][y]:
+            continue
+        
+        max_num = 0
+        max_x, max_y = x, y
+        
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
 
-            count[max_x][max_y] += 1
-            count[x][y] -= 1
+            if in_range(nx, ny) and grid[nx][ny] > max_num:
+                max_num = grid[nx][ny]
+                max_x, max_y = nx, ny
 
-    for x in range(n):
-        for y in range(n):
-            if count[x][y] > 1:
-                count[x][y] = 0
+        count[max_x][max_y] += 1
+        count[x][y] -= 1
+        temp.append((max_x, max_y))
+
+    next_pos = [x for x in temp]
+
+    # for x in range(n):
+    #     for y in range(n):
+    #         if count[x][y] > 1:
+    #             count[x][y] = 0
 
 print(sum([1 if count[x][y] == 1 else 0 for y in range(n) for x in range(n)]))
