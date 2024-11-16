dfd007b9f9cc1a706c0043c91365c37d32e415da
peeerr <ajfeh22@gmail.com>
2024-11-16 18:26:04 +0900

    Title: 벽이 있는 충돌 실험, Time: 513ms, Memory: 27MB, Status: Passed - CodeTree

--- 241116/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 241116/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -3,6 +3,7 @@ def in_range(x, y):
 
 
 T = int(input())
+count = [[0 for _ in range(50)] for _ in range(50)]
 
 for _ in range(T):
     n, m = map(int, input().split())
@@ -10,36 +11,40 @@ for _ in range(T):
     mapper = {'U': 0, 'D': 1, 'L': 2, 'R': 3}
     dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
     
-    count = [[0 for _ in range(n)] for _ in range(n)]
-    marbles = set()
+    marbles = []
     
     # 구슬 정보 초기화
     for _ in range(m):
         x, y, d = input().split()
-        marbles.add((int(x) - 1, int(y) - 1, mapper[d]))
+        marbles.append((int(x) - 1, int(y) - 1, mapper[d]))
     for x, y, _ in marbles:
         count[x][y] = 1
 
     for _ in range(n * 2):
         # 구슬 이동 및 이동된 위치 저장
-        temp = set()
-        for x, y, d in marbles:
+        for i, (x, y, d) in enumerate(marbles):
             nx, ny = x + dxs[d], y + dys[d]
 
             if not in_range(nx, ny):
-                temp.add((x, y, d ^ 1))
+                marbles[i] = (x, y, d ^ 1)
                 continue
             
-            temp.add((nx, ny, d))
+            marbles[i] = (nx, ny, d)
             count[x][y] -= 1
             count[nx][ny] += 1
 
         # 충돌한 구슬 제거
-        marbles = set()
-        for x, y, d in temp:
+        temp = []
+        for x, y, d in marbles:
             if count[x][y] > 1:
                 count[x][y] = 0
             elif count[x][y] == 1:
-                marbles.add((x, y, d))
+                temp.append((x, y, d))
+
+        marbles = temp
 
     print(len(marbles))
+
+    # 한 테스트케이스 끝나면 남은 모든 구슬 제거
+    for x, y, _ in marbles:
+        count[x][y] = 0
