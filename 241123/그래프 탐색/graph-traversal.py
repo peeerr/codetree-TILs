c292919ecd4c5731f9590bd198143db2934ae069
peeerr <ajfeh22@gmail.com>
2024-11-23 22:40:18 +0900

    Title: 그래프 탐색, Time: 111ms, Memory: 20MB, Status: Passed - CodeTree

--- 241123/그래프 탐색/graph-traversal.py
+++ 241123/그래프 탐색/graph-traversal.py
@@ -14,14 +14,13 @@ def dfs(curr_v):
 n, m = map(int, input().split())
 
 graph = defaultdict(list)
+visited = [False for _ in range(n + 1)]
+ans = 0
 
 for _ in range(m):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
-
-visited = [False for _ in range(len(graph) + 1)]
-ans = 0
 
 if m != 0:
     visited[1] = True
