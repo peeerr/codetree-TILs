396fa32e1339ff00fd3c39229d03ac2df67c5986
peeerr <ajfeh22@gmail.com>
2024-11-24 17:04:50 +0900

    Title: 안전 지대, Time: 473ms, Memory: 77MB, Status: Passed - CodeTree

--- 241124/안전 지대/comfort-zone.py
+++ 241124/안전 지대/comfort-zone.py
@@ -1,20 +1,20 @@
-def can_go(x, y):
-    return 0 <= x < n and 0 <= y < m and next_grid[x][y] and not visited[x][y]
+import sys
+sys.setrecursionlimit(2500)
+
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m and next_grid[x][y]
 
 
 def dfs(x, y):
     dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-    stack = [(x, y)]
 
-    while stack:
-        x, y = stack.pop()
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy 
 
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy 
-
-            if can_go(nx, ny):
-                visited[nx][ny] = True
-                stack.append((nx, ny))
+        if in_range(nx, ny):
+            next_grid[nx][ny] = 0
+            dfs(nx, ny)
 
 
 n, m = map(int, input().split())
@@ -25,8 +25,6 @@ ans = 101
 
 for k in range(1, 101):
     next_grid = [[1 for _ in range(m)] for _ in range(n)]
-    visited = [[False for _ in range(m)] for _ in range(n)]
-
     area = 0
     
     for x in range(n):
@@ -36,9 +34,8 @@ for k in range(1, 101):
 
     for x in range(n):
         for y in range(m):
-            if can_go(x, y):
+            if next_grid[x][y]:
                 area += 1
-                visited[x][y] = True
                 dfs(x, y)
 
     if max_area < area:
