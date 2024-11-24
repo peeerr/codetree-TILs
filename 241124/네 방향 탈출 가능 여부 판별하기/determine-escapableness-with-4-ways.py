8673e3d4a60acc0466ab86d5073797c4d7471855
peeerr <ajfeh22@gmail.com>
2024-11-24 18:24:57 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 71ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241124/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,26 @@
+def can_go(x, y):
+    return 0 <= x < n and 0 <= y < m and grid[x][y] and not visited[x][y]
+
+
+def bfs(x, y):
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+    q = [(x, y)]
+
+    while q:
+        x, y = q.popleft()
+        
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+
+            if can_go(nx, ny):
+                visited[nx][ny] = True
+                q.append((nx, ny))
+
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[0 for _ in range(m)] for _ in range(n)]
+bfs(0, 0)
+
+print(visited[n - 1][m - 1])
