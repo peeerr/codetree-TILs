47f2373e78d0a569c02bd9a7658efc1d49409be5
peeerr <ajfeh22@gmail.com>
2024-11-25 16:11:02 +0900

    Title: K번 최댓값으로 이동하기, Time: 197ms, Memory: 23MB, Status: Wrong Answer - CodeTree

---
+++ 241125/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+
+def can_go(x, y, num):
+    return 0 <= x < n and 0 <= y < n and not visited[x][y] and grid[x][y] < num
+
+
+def bfs(num):
+    global is_go
+    visited[q[0][0]][q[0][1]] = True
+
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+    max_num = 0
+    max_x, max_y = n, n
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+
+            if can_go(nx, ny, num):
+                is_go = True
+                visited[nx][ny] = True
+                q.append((nx, ny))
+
+                if grid[nx][ny] > max_num:
+                    max_num = grid[nx][ny]
+                    max_x, max_y = nx, ny
+                elif grid[nx][ny] == max_num:
+                    if nx < max_x:
+                        max_x, max_y = nx, ny
+                    elif nx == max_x:
+                        if ny < max_x:
+                            max_x, max_y = nx, ny
+
+    return max_x, max_y
+
+
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+x, y = map(lambda x : int(x) - 1, input().split())
+
+ans_x, ans_y = 0, 0
+
+for _ in range(k):
+    visited = [[False for _ in range(n)] for _ in range(n)]
+    q = deque([(x, y)])
+
+    is_go = False
+    visited[x][y] = True
+    max_x, max_y = bfs(grid[x][y])
+
+    if is_go:
+        ans_x, ans_y = max_x, max_y
+        x, y = max_x, max_y
+    else:
+        break
+
+print(ans_x + 1, ans_y + 1)
