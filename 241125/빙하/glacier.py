f823b66bd6d30f5dcdf12af5786b709ebcb88960
peeerr <ajfeh22@gmail.com>
2024-11-25 20:15:34 +0900

    Title: 빙하, Time: 1137ms, Memory: 25MB, Status: Passed - CodeTree

--- 241125/빙하/glacier.py
+++ 241125/빙하/glacier.py
@@ -1,34 +1,13 @@
 from collections import deque
 
 
-def have_effect():
-    for i in range(n):
-        for j in range(m):
-            if (i == 0 or i == n - 1 or j == 0 or j == m - 1) and visited[i][j]:
-                return True
-    return False
-
-
-def record(visited):
-    for i in range(n):
-        for j in range(m):
-            if visited[i][j]:
-                water_have_effect[i][j] = True
-
-def init():
-    for i in range(n):
-        for j in range(m):
-            if i == 0 or i == n - 1 or j == 0 or j == m - 1:
-                water_have_effect[i][j] = True
-
-
 def melt():
     global cnt
     dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 
     for x in range(n):
         for y in range(m):
-            if water_have_effect[x][y]:
+            if visited[x][y]:
                 for dx, dy in zip(dxs, dys):
                     nx, ny = x + dx, y + dy
                     if in_range(nx, ny) and grid[nx][ny]:
@@ -73,22 +52,12 @@ time = 0
 last_melted = 0
 
 for t in range(1, max(n, m)):
-    water_have_effect = [[False for _ in range(m)] for _ in range(n)]
-    init()
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    visited[0][0] = True
+    q = deque([(0, 0)])
 
-    for i in range(n):
-        for j in range(m):
-            if i == 0 or i == n - 1 or j == 0 or j == m - 1 or grid[i][j]:
-                continue
-            
-            visited = [[False for _ in range(m)] for _ in range(n)]
-            visited[i][j] = True
-            q = deque([(i, j)])
-            bfs()
-            
-            if have_effect():
-                record(visited)
-                
+    bfs()
+
     cnt = 0
     melt()
 
