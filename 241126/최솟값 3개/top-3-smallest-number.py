742fc56ae0dfde47f697393a308e0a38ba6f67ee
peeerr <ajfeh22@gmail.com>
2024-11-26 19:10:55 +0900

    Title: 최솟값 3개, Time: 115ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241126/최솟값 3개/top-3-smallest-number.py
+++ 241126/최솟값 3개/top-3-smallest-number.py
@@ -1,15 +1,14 @@
-def get_three_smallest_product(numbers):
-    if len(numbers) < 3:
-        return -1
-        
-    sorted_nums = sorted(numbers)
-    return sorted_nums[0] * sorted_nums[1] * sorted_nums[2]
+import heapq
 
 n = int(input())
-numbers = list(map(int, input().split()))
+nums = list(map(int, input().split()))
 
-current_numbers = []
-for num in numbers:
-    current_numbers.append(num)
-    result = get_three_smallest_product(current_numbers)
-    print(result)
+pq = []
+
+for num in nums:
+    heapq.heappush(pq, num)
+
+    if len(pq) < 3:
+        print(-1)
+    else:
+        print(pq[0] * pq[1] * pq[2])
