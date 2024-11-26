40e83aad236e70f1245bb6712c938b2af249f156
peeerr <ajfeh22@gmail.com>
2024-11-26 23:08:13 +0900

    Title: 최대 숫자 만들기, Time: 227ms, Memory: 24MB, Status: Runtime Error - CodeTree

---
+++ 241126/최대 숫자 만들기/make-biggest-num.py
@@ -0,0 +1,25 @@
+from functools import cmp_to_key
+
+
+def compare(a, b):
+    if len(str(a)) == len(str(b)):
+        if a > b:
+            return -1
+        elif a < b:
+            return 1
+        else:
+            0
+    else:
+        if int(str(a) + str(b)) > int(str(b) + str(a)):
+            return -1
+        elif int(str(a) + str(b)) < int(str(b) + str(a)):
+            return 1
+        else:
+            0
+
+
+n = int(input())
+nums = [int(input()) for _ in range(n)]
+nums.sort(key=cmp_to_key(compare))
+
+print("".join(map(str, nums)))
