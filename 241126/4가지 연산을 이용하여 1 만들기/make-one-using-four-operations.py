29353634ffba321c80ca1cf7f1ee6ed957bd5909
peeerr <ajfeh22@gmail.com>
2024-11-26 15:44:17 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 1479ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 241126/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 241126/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -3,20 +3,19 @@ from collections import deque
 
 
 def bfs():
-    ans = sys.maxsize
-
     while q:
         n, cnt = q.popleft()
 
         if n == 1:
-            ans = min(ans, cnt)
-        elif n % 3 == 0 and n % 2 == 0:
-            q.append((n // 3, cnt + 1))
+            return cnt
+        elif n % 2 == 0:
+            q.append((n + 1, cnt + 1))
+            q.append((n - 1, cnt + 1))
             q.append((n // 2, cnt + 1))
         elif n % 3 == 0:
+            q.append((n + 1, cnt + 1))
+            q.append((n - 1, cnt + 1))
             q.append((n // 3, cnt + 1))
-        elif n % 2 == 0:
-            q.append((n // 2, cnt + 1))
         else:
             q.append((n + 1, cnt + 1))
             q.append((n - 1, cnt + 1))
