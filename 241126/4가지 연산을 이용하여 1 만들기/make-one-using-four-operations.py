8504886f98d6689fe55ea4238f81536e90298eb9
peeerr <ajfeh22@gmail.com>
2024-11-26 15:47:01 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 61ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241126/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 241126/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -3,22 +3,40 @@ from collections import deque
 
 
 def bfs():
+    visited = set()
+
     while q:
         n, cnt = q.popleft()
 
         if n == 1:
             return cnt
         elif n % 2 == 0:
-            q.append((n + 1, cnt + 1))
-            q.append((n - 1, cnt + 1))
-            q.append((n // 2, cnt + 1))
+            if n + 1 not in visited:
+                q.append((n + 1, cnt + 1))
+                visited.add(n + 1)
+            if n - 1 not in visited:
+                q.append((n - 1, cnt + 1))
+                visited.add(n - 1)
+            if n // 2 not in visited:
+                q.append((n // 2, cnt + 1))
+                visited.add(n // 2)
         elif n % 3 == 0:
-            q.append((n + 1, cnt + 1))
-            q.append((n - 1, cnt + 1))
-            q.append((n // 3, cnt + 1))
+            if n + 1 not in visited:
+                q.append((n + 1, cnt + 1))
+                visited.add(n + 1)
+            if n - 1 not in visited:
+                q.append((n - 1, cnt + 1))
+                visited.add(n - 1)
+            if n // 3 not in visited:
+                q.append((n // 3, cnt + 1))
+                visited.add(n // 3)
         else:
-            q.append((n + 1, cnt + 1))
-            q.append((n - 1, cnt + 1))
+            if n + 1 not in visited:
+                q.append((n + 1, cnt + 1))
+                visited.add(n + 1)
+            if n - 1 not in visited:
+                q.append((n - 1, cnt + 1))
+                visited.add(n - 1)
     
     return ans
 
