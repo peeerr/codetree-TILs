56e0293274bb96149c64fd7bf32930e391ea0208
peeerr <ajfeh22@gmail.com>
2024-11-26 15:21:23 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 86ms, Memory: 20MB, Status: Wrong Answer - CodeTree

--- 241126/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 241126/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -10,6 +10,9 @@ def bfs():
 
         if n == 1:
             ans = min(ans, cnt)
+        elif n % 3 == 0 and n % 2 == 0:
+            q.append((n / 3, cnt + 1))
+            q.append((n / 2, cnt + 1))
         elif n % 3 == 0:
             q.append((n / 3, cnt + 1))
         elif n % 2 == 0:
