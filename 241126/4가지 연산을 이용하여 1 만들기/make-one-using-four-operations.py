ce2049d7224b7a64eb9b8a731883016cb67757ab
peeerr <ajfeh22@gmail.com>
2024-11-26 15:51:34 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 65ms, Memory: 17MB, Status: Passed - CodeTree

--- 241126/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 241126/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -8,48 +8,29 @@ def bfs():
     while q:
         n, cnt = q.popleft()
 
+        if n in visited:
+            continue
+        
+        visited.add(n)
+
         if n == 1:
             return cnt
         elif n % 2 == 0 and n % 3 == 0:
-            if n + 1 not in visited:
-                q.append((n + 1, cnt + 1))
-                visited.add(n + 1)
-            if n - 1 not in visited:
-                q.append((n - 1, cnt + 1))
-                visited.add(n - 1)
-            if n // 2 not in visited:
-                q.append((n // 2, cnt + 1))
-                visited.add(n // 2)
-            if n // 3 not in visited:
-                q.append((n // 3, cnt + 1))
-                visited.add(n // 3)
+            q.append((n + 1, cnt + 1))
+            q.append((n - 1, cnt + 1))
+            q.append((n // 2, cnt + 1))
+            q.append((n // 3, cnt + 1))
         elif n % 2 == 0:
-            if n + 1 not in visited:
-                q.append((n + 1, cnt + 1))
-                visited.add(n + 1)
-            if n - 1 not in visited:
-                q.append((n - 1, cnt + 1))
-                visited.add(n - 1)
-            if n // 2 not in visited:
-                q.append((n // 2, cnt + 1))
-                visited.add(n // 2)
+            q.append((n + 1, cnt + 1))
+            q.append((n - 1, cnt + 1))
+            q.append((n // 2, cnt + 1))
         elif n % 3 == 0:
-            if n + 1 not in visited:
-                q.append((n + 1, cnt + 1))
-                visited.add(n + 1)
-            if n - 1 not in visited:
-                q.append((n - 1, cnt + 1))
-                visited.add(n - 1)
-            if n // 3 not in visited:
-                q.append((n // 3, cnt + 1))
-                visited.add(n // 3)
+            q.append((n + 1, cnt + 1))
+            q.append((n - 1, cnt + 1))
+            q.append((n // 3, cnt + 1))
         else:
-            if n + 1 not in visited:
-                q.append((n + 1, cnt + 1))
-                visited.add(n + 1)
-            if n - 1 not in visited:
-                q.append((n - 1, cnt + 1))
-                visited.add(n - 1)
+            q.append((n + 1, cnt + 1))
+            q.append((n - 1, cnt + 1))
     
     return ans
 
@@ -58,3 +39,67 @@ n = int(input())
 q = deque([(n, 0)])
 
 print(bfs())
+
+
+
+
+# import sys
+# from collections import deque
+
+
+# def bfs():
+#     visited = set()
+
+#     while q:
+#         n, cnt = q.popleft()
+
+#         if n == 1:
+#             return cnt
+#         elif n % 2 == 0 and n % 3 == 0:
+#             if n + 1 not in visited:
+#                 q.append((n + 1, cnt + 1))
+#                 visited.add(n + 1)
+#             if n - 1 not in visited:
+#                 q.append((n - 1, cnt + 1))
+#                 visited.add(n - 1)
+#             if n // 2 not in visited:
+#                 q.append((n // 2, cnt + 1))
+#                 visited.add(n // 2)
+#             if n // 3 not in visited:
+#                 q.append((n // 3, cnt + 1))
+#                 visited.add(n // 3)
+#         elif n % 2 == 0:
+#             if n + 1 not in visited:
+#                 q.append((n + 1, cnt + 1))
+#                 visited.add(n + 1)
+#             if n - 1 not in visited:
+#                 q.append((n - 1, cnt + 1))
+#                 visited.add(n - 1)
+#             if n // 2 not in visited:
+#                 q.append((n // 2, cnt + 1))
+#                 visited.add(n // 2)
+#         elif n % 3 == 0:
+#             if n + 1 not in visited:
+#                 q.append((n + 1, cnt + 1))
+#                 visited.add(n + 1)
+#             if n - 1 not in visited:
+#                 q.append((n - 1, cnt + 1))
+#                 visited.add(n - 1)
+#             if n // 3 not in visited:
+#                 q.append((n // 3, cnt + 1))
+#                 visited.add(n // 3)
+#         else:
+#             if n + 1 not in visited:
+#                 q.append((n + 1, cnt + 1))
+#                 visited.add(n + 1)
+#             if n - 1 not in visited:
+#                 q.append((n - 1, cnt + 1))
+#                 visited.add(n - 1)
+    
+#     return ans
+
+
+# n = int(input())
+# q = deque([(n, 0)])
+
+# print(bfs())
