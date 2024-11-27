31416f1fb8e6c8b049fba2d1b94790cfcd6fe03d
peeerr <ajfeh22@gmail.com>
2024-11-27 19:18:13 +0900

    Title: 삼 오 무, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241127/삼 오 무/three-five-moo.py
+++ 241127/삼 오 무/three-five-moo.py
@@ -2,30 +2,19 @@ def calc(num):
     return num - (num // 3 + num // 5 - num // 15)
 
 
-def binary_search():
-    left, right = 1, 10 ** 9
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        while mid % 3 == 0 or mid % 5 == 0:
-            mid += 1
-            if mid > right:
-                break
-
-        if mid > right:
-            right = left - 1
-            continue
-
-        idx = calc(mid)
-
-        if idx == n:
-            return mid
-        elif idx > n:
-            right = mid - 1
-        else:
-            left = mid + 1
-
-
 n = int(input())
-print(binary_search())
+
+ans = 10 ** 9
+left, right = 1, 10 ** 9
+
+while left <= right:
+    mid = (left + right) // 2
+    idx = calc(mid)
+
+    if idx >= n:
+        right = mid - 1
+        ans = min(ans, mid)
+    else:
+        left = mid + 1
+
+print(ans)
