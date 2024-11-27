5ffa20cb689843ee50230b154f31a2e6e91af0b1
peeerr <ajfeh22@gmail.com>
2024-11-27 18:55:45 +0900

    Title: 삼 오 무, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241127/삼 오 무/three-five-moo.py
+++ 241127/삼 오 무/three-five-moo.py
@@ -7,8 +7,18 @@ def binary_search():
 
     while left <= right:
         mid = (left + right) // 2
+
+        while mid % 3 == 0 or mid % 5 == 0:
+            mid += 1
+            if mid > right:
+                break
+
+        if mid > right:
+            right = left - 1
+            continue
+
         idx = calc(mid)
-        
+
         if idx == n:
             return mid
         elif idx > n:
