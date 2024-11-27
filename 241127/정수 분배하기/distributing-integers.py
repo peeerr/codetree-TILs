7c209df951b53c8bc1d4dc48334c22737648855e
peeerr <ajfeh22@gmail.com>
2024-11-27 16:47:35 +0900

    Title: 정수 분배하기, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241127/정수 분배하기/distributing-integers.py
@@ -0,0 +1,22 @@
+def possible(k):
+    a = 0
+    for x in arr:
+        a += x // k
+    return a >= m
+
+
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+ans = 0
+left, right = 0, max(arr)
+
+while left <= right:
+    mid = (left + right) // 2
+    if possible(mid):
+        ans = mid
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(ans)
