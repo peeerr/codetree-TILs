ae70cdc5990229d3a6cf25189b60c9a307853b9e
peeerr <ajfeh22@gmail.com>
2024-11-27 12:17:04 +0900

    Title: 가장 먼저 나오는 숫자, Time: 5066ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

--- 241127/가장 먼저 나오는 숫자/first-appear-number.py
+++ 241127/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,33 +1,33 @@
-def lower(target):
-    left, right = 0, n - 1
-    min_idx = n
+# def lower(target):
+#     left, right = 0, n - 1
+#     min_idx = n
 
-    while left <= right:
-        mid = (left + right) // 2
+#     while left <= right:
+#         mid = (left + right) // 2
 
-        if arr[mid] >= target:
-            min_idx = min(min_idx, mid)
-            right = mid - 1
-        else:
-            left = mid + 1
+#         if arr[mid] >= target:
+#             min_idx = min(min_idx, mid)
+#             right = mid - 1
+#         else:
+#             left = mid + 1
 
-    return min_idx
+#     return min_idx
 
 
-def upper(target):
-    left, right = 0, n - 1
-    min_idx = n
+# def upper(target):
+#     left, right = 0, n - 1
+#     min_idx = n
 
-    while left <= right:
-        mid = (left + right) // 2
+#     while left <= right:
+#         mid = (left + right) // 2
 
-        if arr[mid] > target:
-            min_idx = min(min_idx, mid)
-            right = mid - 1
-        else:
-            left = mid + 1
+#         if arr[mid] > target:
+#             min_idx = min(min_idx, mid)
+#             right = mid - 1
+#         else:
+#             left = mid + 1
 
-    return min_idx
+#     return min_idx
 
 
 n, m = map(int, input().split())
@@ -35,9 +35,13 @@ arr = list(map(int, input().split()))
 params = list(map(int, input().split()))
 
 for param in params:
-    lower_idx, upper_idx = lower(param), upper(param)
-
-    if upper_idx - lower_idx:
-        print(lower_idx + 1)
+    if param in arr:
+        print(arr.index(param) + 1)
     else:
         print(-1)
+    # lower_idx, upper_idx = lower(param), upper(param)
+
+    # if upper_idx - lower_idx:
+    #     print(lower_idx + 1)
+    # else:
+    #     print(-1)
