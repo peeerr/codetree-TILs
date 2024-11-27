ac3b35209b8fd7e2738d2c813f6606da10fff1dc
peeerr <ajfeh22@gmail.com>
2024-11-27 12:15:54 +0900

    Title: 가장 먼저 나오는 숫자, Time: 182ms, Memory: 29MB, Status: Passed - CodeTree

--- 241127/가장 먼저 나오는 숫자/first-appear-number.py
+++ 241127/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,16 +1,33 @@
-def binary_search(target):
+def lower(target):
     left, right = 0, n - 1
     min_idx = n
 
     while left <= right:
         mid = (left + right) // 2
+
         if arr[mid] >= target:
-            idx = min(min_idx, mid)
+            min_idx = min(min_idx, mid)
             right = mid - 1
         else:
             left = mid + 1
 
-    return idx
+    return min_idx
+
+
+def upper(target):
+    left, right = 0, n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] > target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return min_idx
 
 
 n, m = map(int, input().split())
@@ -18,8 +35,9 @@ arr = list(map(int, input().split()))
 params = list(map(int, input().split()))
 
 for param in params:
-    idx = binary_search(param)
-    if arr[idx] == param:
-        print(idx + 1)
+    lower_idx, upper_idx = lower(param), upper(param)
+
+    if upper_idx - lower_idx:
+        print(lower_idx + 1)
     else:
         print(-1)
