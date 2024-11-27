f12a7f20335419816a6077d831953fae7a8f88b5
peeerr <ajfeh22@gmail.com>
2024-11-27 19:18:54 +0900

    Title: 삼 오 무, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241127/삼 오 무/three-five-moo.py
+++ 241127/삼 오 무/three-five-moo.py
@@ -1,11 +1,16 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+
 def calc(num):
     return num - (num // 3 + num // 5 - num // 15)
 
 
 n = int(input())
 
-ans = 10 ** 9
-left, right = 1, 10 ** 9
+ans = INT_MAX
+left, right = 1, INT_MAX
 
 while left <= right:
     mid = (left + right) // 2
