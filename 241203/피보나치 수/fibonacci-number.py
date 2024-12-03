6c5bd835e6cd5cebd87c163687e37e2f55b2e78f
peeerr <ajfeh22@gmail.com>
2024-12-03 16:56:07 +0900

    Title: 피보나치 수, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241203/피보나치 수/fibonacci-number.py
+++ 241203/피보나치 수/fibonacci-number.py
@@ -1,10 +1,10 @@
 n = int(input())
 
 d = [0 for _ in range(n + 1)]
+d[0] = 0
 d[1] = 1
-d[2] = 1
 
-for i in range(3, n + 1):
+for i in range(2, n + 1):
     d[i] = d[i - 1] + d[i - 2]
 
 print(d[n])
