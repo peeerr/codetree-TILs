988c1fa92f095ec5b4bd7d627244e7d96aae41d2
peeerr <ajfeh22@gmail.com>
2024-12-03 20:33:07 +0900

    Title: 사각형 채우기 3, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241203/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,15 @@
+MOD = 1000000007
+
+
+n = int(input())
+dp = [0 for _ in range(n + 1)]
+
+dp[0] = 1
+dp[1] = 2
+
+for i in range(2, n + 1):
+    dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD
+    for j in range(i - 3, -1, -1):
+        dp[i] = (dp[i] + dp[j] * 2) % MOD
+
+print(dp[n])
