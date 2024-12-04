87f3995780c5e7f754c12ea171a15fa671346835
peeerr <ajfeh22@gmail.com>
2024-12-04 17:08:00 +0900

    Title: 배낭 채우기, Time: 54ms, Memory: 17MB, Status: Passed - CodeTree

--- 241204/배낭 채우기/knapsack.py
+++ 241204/배낭 채우기/knapsack.py
@@ -1,14 +1,9 @@
 n, m = map(int, input().split())
-infos = [0] + [tuple(map(int, input().split())) for _ in range(n)]
+dp = [0] * (m + 1)
 
-d = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+for _ in range(n):
+    w, v = map(int, input().split())
+    for j in range(m, w-1, -1):  
+        dp[j] = max(dp[j], dp[j-w] + v)
 
-for i in range(1, n + 1):
-    w, v = infos[i]
-    for j in range(1, m + 1):
-        if j >= w:
-            d[i][j] = max(d[i - 1][j], d[i - 1][j - w] + v)
-        else:
-            d[i][j] = d[i - 1][j]
-
-print(d[n][m])
+print(dp[m])
