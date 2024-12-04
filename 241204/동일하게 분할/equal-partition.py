6483d6b9358c683e8702b2fe2389bbd3ed50c01a
peeerr <ajfeh22@gmail.com>
2024-12-04 17:50:39 +0900

    Title: 동일하게 분할, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241204/동일하게 분할/equal-partition.py
+++ 241204/동일하게 분할/equal-partition.py
@@ -7,6 +7,6 @@ dp = [0 for _ in range(m + 1)]
 for x in arr:
     for i in range(m, -1, -1):
         if i >= x:
-            dp[i] = max(dp[i], dp[i - x] + x)
+            dp[i] = min(dp[i], dp[i - x] + x)
 
 print('Yes' if dp[m] == m else 'No')
