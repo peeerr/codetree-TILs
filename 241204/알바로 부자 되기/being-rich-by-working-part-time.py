69cd6da1213413618060540bce9c484cc2309dfb
peeerr <ajfeh22@gmail.com>
2024-12-04 14:20:20 +0900

    Title: 알바로 부자 되기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241204/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -0,0 +1,17 @@
+def is_overlap(s1, e1, s2, e2):
+    return e1 == s2 or e2 == s1 or s1 <= s2 <= e1 or s1 <= e2 <= e1
+
+
+n = int(input())
+infos = [(0, 0, 0)] + [tuple(map(int, input().split())) for _ in range(n)]
+
+dp = [0 for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    s1, e1, p1 = infos[i]
+    for j in range(i):
+        s2, e2, _ = infos[j]
+        if not is_overlap(s1, e1, s2, e2):
+            dp[i] = max(dp[i], dp[j] + p1)
+
+print(max(dp))
