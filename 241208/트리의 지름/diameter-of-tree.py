9fa5fdec530e1fbe2b30d566c2bf8ec37c880c83
peeerr <ajfeh22@gmail.com>
2024-12-08 22:27:36 +0900

    Title: 트리의 지름, Time: 379ms, Memory: 39MB, Status: Runtime Error - CodeTree

---
+++ 241208/트리의 지름/diameter-of-tree.py
@@ -0,0 +1,35 @@
+def dfs(start, total_dist):
+    for v, w in tree[start]:
+        if not visited[v]:
+            visited[v] = True
+            dist[v] = total_dist + w
+            dfs(v, total_dist + w)
+
+
+n = int(input())
+tree = [[] for _ in range(n + 1)]
+
+for _ in range(n - 1):
+    u, v, w = map(int, input().split())
+    tree[u].append((v, w))
+    tree[v].append((u, w))
+
+dist = [0 for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
+
+visited[1] = True
+dfs(1, 0)
+
+max_v, max_dist = 0, 0
+for i in range(1, n + 1):
+    if dist[i] > max_dist:
+        max_dist = dist[i]
+        max_v = i
+
+dist = [0 for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
+
+visited[max_v] = True
+dfs(max_v, 0)
+
+print(max(dist))
