5e63e06c4a06328784f1c50993841f6bd964d0c9
peeerr <ajfeh22@gmail.com>
2024-12-08 22:58:55 +0900

    Title: 트리 노드 제거, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241208/트리 노드 제거/remove-tree-node.py
@@ -0,0 +1,30 @@
+def remove(start):
+    for v in tree[start]:
+        if not visited[v]:
+            visited[v] = True
+            remove(v)
+
+
+n = int(input())
+parents = list(map(int, input().split()))
+tree = [[] for _ in range(n)]
+
+for child, parent in enumerate(parents):
+    if parent == -1:
+        continue
+    tree[parent].append(child)
+
+x = int(input())
+visited = [False for _ in range(n)]
+visited[x] = True
+remove(x)
+
+ans = 0
+
+for i in range(n):
+    if visited[i]:
+        continue
+    if not len(tree[i]):
+        ans += 1
+
+print(ans)
