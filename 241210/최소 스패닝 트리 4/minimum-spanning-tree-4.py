750751c9c2f28f9ce14c69f87827ed4ed8dd4965
peeerr <ajfeh22@gmail.com>
2024-12-10 20:07:01 +0900

    Title: 최소 스패닝 트리 4, Time: 464ms, Memory: 34MB, Status: Passed - CodeTree

--- 241210/최소 스패닝 트리 4/minimum-spanning-tree-4.py
+++ 241210/최소 스패닝 트리 4/minimum-spanning-tree-4.py
@@ -15,11 +15,12 @@ v_type = [''] + input().split()
 edges = sorted([list(map(int, input().split())) for _ in range(m)], key=lambda x: x[2])
 
 uf = [i for i in range(n + 1)]
-ans = 0
+ans, selected_edge_cnt = 0, 0
 
 for v, u, w in edges:
     if find(v) != find(u) and v_type[v] != v_type[u]:
         ans += w
+        selected_edge_cnt += 1
         union(v, u)
 
-print(ans)
+print(ans if selected_edge_cnt == n - 1 else -1)
