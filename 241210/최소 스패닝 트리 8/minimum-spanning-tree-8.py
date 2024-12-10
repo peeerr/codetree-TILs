294ae29799d8ab45698e77f3a1153f1a89cb21f5
peeerr <ajfeh22@gmail.com>
2024-12-10 21:09:21 +0900

    Title: 최소 스패닝 트리 8, Time: 83ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 241210/최소 스패닝 트리 8/minimum-spanning-tree-8.py
@@ -0,0 +1,31 @@
+import sys, heapq
+
+INT_MAX = sys.maxsize
+
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    u, v, w = map(int, input().split())
+    graph[v].append((u, w))
+    graph[u].append((v, w))
+
+dist = [INT_MAX for _ in range(n + 1)]
+
+# 아무 정점에서 시작
+dist[1] = 0
+pq = [(0, 1)]
+
+while pq:
+    min_dist, min_v = heapq.heappop(pq)
+
+    if dist[min_v] != min_dist:
+        continue
+
+    for u, w in graph[min_v]:
+        if w < dist[u]:
+            dist[u] = w
+            heapq.heappush(pq, (w, u))
+
+print(sum(dist[2:]))
