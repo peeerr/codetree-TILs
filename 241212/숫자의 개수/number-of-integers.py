116431949f098e8cbaffa37f32a2d3e46bfd81f3
peeerr <ajfeh22@gmail.com>
2024-12-12 16:09:45 +0900

    Title: 숫자의 개수, Time: 283ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 241212/숫자의 개수/number-of-integers.py
@@ -0,0 +1,36 @@
+def upper_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return min_idx
+
+
+def lower_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+            
+    return min_idx
+
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+for _ in range(m):
+    target = int(input())
+    print(upper_bound(target) - lower_bound(target))
